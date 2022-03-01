5a68f553e00514d60c5bc9718be888ba72ecd2d2
UMHUK90 <40740079+UMHUK90@users.noreply.github.com>
2022-03-01 17:09:40 +0300

    Add files via upload

---
+++ index-AG.js
@@ -0,0 +1,13 @@
+const privateCircle = document.querySelector(".locked-pane");
+const activeLink = document.querySelector('a.nav-link--active span.space__name');
+
+function changeVisibility() {
+    if (privateCircle !== null) {
+        activeLink.style.setProperty("--lock-icon-visibility", "visible");
+      } else {
+        activeLink.style.setProperty("--lock-icon-visibility", "hidden");
+  }
+}
+window.setTimeout(() => {
+  changeVisibility();
+}, 200);
